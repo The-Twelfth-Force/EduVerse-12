22b8ccfeb7df6609223a2f67825118c44b6dc834
hassetyo <hassetyoseph1@gmail.com>
2025-04-13 05:19:20 -0500

    simple cart and registration functionality

--- frontend/src/app/registration/cart/page.tsx
+++ frontend/src/app/registration/cart/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import ScheduleCalendar from '@/components/Planner/ScheduleCalendar'
 import { CourseSection } from '@/types/course'
+import { Button } from '@/components/ui/button'
 
 export default function Cart() {
   const [selectedSections, setSelectedSections] = useState<CourseSection[]>([])
@@ -14,8 +15,33 @@ export default function Cart() {
   }, [])
 
   return (
-    <div className="aspect-ratio: auto relative border-gray-200 border-[1px] rounded-md w-full overflow-clip shadow-md">
-      <ScheduleCalendar courses={selectedSections} />
+    <div>
+      <div className="sticky flex justify-center w-full h-fill m-1">
+        <Button className="w-full h-fill" 
+          onClick={() => {
+            const existing = localStorage.getItem('registeredClasses')
+
+            if (existing) {
+              const alreadyRegistered: CourseSection[] = JSON.parse(existing)
+              const newOnes = selectedSections.filter(
+                section => !alreadyRegistered.some(
+                  reg => reg._id === section._id
+                )
+              )
+              const updated = [...alreadyRegistered, ...newOnes]
+              localStorage.setItem('registeredClasses', JSON.stringify(updated))
+            } else {
+              localStorage.setItem('registeredClasses', JSON.stringify(selectedSections))
+            }
+
+            localStorage.setItem('selectedSections', '')
+          }} >
+          Register
+        </Button>
+      </div>
+      <div className="aspect-ratio: auto relative border-gray-200 border-[1px] rounded-md w-full overflow-clip shadow-md">
+          <ScheduleCalendar courses={selectedSections} />
+      </div>
     </div>
   )
 }
